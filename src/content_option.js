34e5a87268619a7794f37e0a11aa702bba8014ad
Hafiz Saad Malik <137260149+SaadMalikHajex@users.noreply.github.com>
2024-06-26 23:08:22 +0500

    portfolio added

--- src/content_option.js
+++ src/content_option.js
@@ -16,7 +16,7 @@ const introdata = {
 };
 
 const dataabout = {
-    title: "abit about my self",
+    title: "about my self",
     aboutme: "Hello, I'm Hafiz Saad Malik, a seasoned front-end developer with over 9 years of experience in crafting engaging and dynamic web experiences. My journey in web development began with a passion for creating visually stunning and user-friendly interfaces. Over the years, I have honed my skills in CMS development, mastering the art of delivering seamless and efficient content management solutions. I have also worked extensively with various platforms and frameworks, including Shopify, WordPress, BigCommerce, React.js, and most CSS frameworks. My commitment to excellence and innovation has led me to work on a variety of projects, each presenting unique challenges that I tackle with enthusiasm and expertise. I am excited to continue pushing the boundaries of web development and creating impactful digital experiences.",
 };
 const worktimeline = [{
@@ -73,62 +73,36 @@ const services = [{
 ];
 
 const dataportfolio = [{
-        img: "https://lumaandleaf.com/cdn/shop/files/Fresh_Lift_1080x1080_web_size_540x540.png",
+        img: "//lumaandleaf.com/cdn/shop/files/logo_ll.png?v=1646921965",
         description: "Luma & Leaf",
         link: "https://lumaandleaf.com/",
     },
     {
-        img: "https://cdn.shopify.com/s/files/1/1219/6322/files/android-chrome-512x512.png?v=1677220934",
+        img: "https://www.lussoleather.com/cdn/shop/files/logo_1_cut_48251503-2f6d-4f01-808d-088d6455764e.jpg",
         description: "Lusso Leather",
         link: "https://www.lussoleather.com/",
     },
     {
-        img: "https://www.sparkset.io/wp-content/uploads/2023/01/Logo1.png",
+        img: "https://assets.softr-files.com/applications/4986d290-b229-4b38-adfd-82dd5e8ec286/assets/124b550e-1346-4fd0-a1f4-ed080def6608.png",
         description: "Sparkset",
         link: "https://www.sparkset.io/",
     },
+    {
+        img: "https://www.velasailingsupply.com/images/stencil/original/image-manager/vela101223.png",
+        description: "Vela Sailing Supply",
+        link: "https://www.velasailingsupply.com",
+    },
+    {
+        img: "https://i0.wp.com/bayoubeats.biz/wp-content/uploads/2023/05/cropped-beatsbeats-icon.png?fit=180%2C180&ssl=1",
+        description: "Bayou Beats",
+        link: "https://shop.bayoubeats.biz/",
+    },
+    {
+        img: "https://tokyointernationaljp.com/wp-content/uploads/2023/01/logo-1-removebg-preview.png",
+        description: "Tokyo International",
+        link: "https://tokyointernationaljp.com/",
+    },
     
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
 ];
 
 const contactConfig = {
